a165d7da09aff82e7d4c15b7849409972598738c
Daniel Leavitt <danleavitt0@gmail.com>
2014-07-29 11:58:16 -0700

    prerender fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Start sails and pass it command line arguments
 var express = require('express');
-var app = module.exports = express();
 require('sails').lift(require('optimist').argv);
+var app = module.exports = express();
 app.use(require('prerender-node').set('prerenderToken', 'toId3gs33teeQt7Ln7XR'));
